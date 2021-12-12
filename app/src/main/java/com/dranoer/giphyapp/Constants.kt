cb01b36ad097e68511a19d7b5b9221f3a6502d96
dranoer <dranoer@gmail.com>
2021-12-13 00:21:49 +0330

    refactor: Resources

--- app/src/main/java/com/dranoer/giphyapp/Constants.kt
+++ app/src/main/java/com/dranoer/giphyapp/Constants.kt
@@ -5,5 +5,8 @@ object Constants {
     const val BASE_URL = "https://api.giphy.com/v1/"
     const val API_KEY = "kq8X4RnlB3hitketgUSjbqiKNUXlGkJb"
 
+    const val MAIN_TAB = "Main"
+    const val FAV_TAB = "Fav"
+
     const val DATABASE_NAME = "giphy"
 }
--- app/src/main/java/com/dranoer/giphyapp/MainActivity.kt
+++ app/src/main/java/com/dranoer/giphyapp/MainActivity.kt
@@ -2,15 +2,14 @@ package com.dranoer.giphyapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.dranoer.giphyapp.Constants.FAV_TAB
+import com.dranoer.giphyapp.Constants.MAIN_TAB
 import com.dranoer.giphyapp.databinding.ActivityMainBinding
 import com.dranoer.giphyapp.ui.ViewPagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
-val tabArray = arrayOf(
-    "Main",
-    "Fav",
-)
+val tabArray = arrayOf(MAIN_TAB, FAV_TAB)
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
